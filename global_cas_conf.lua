f5c79defe2b3aa4f2d23355403195431ac6a3e9f
ezhangjun <ezhangjun@qq.com>
2020-03-22 12:29:55 +0800

    Add files via upload

---
+++ global_cas_conf.lua
@@ -0,0 +1,8 @@
+return {
+   cas_uri = "http://sso.cas-server.com";   
+
+   -- store_name = "cas_store";
+   -- session_lifetime = 3600;
+   -- cookie_name = "NGXCAS";
+   -- cookie_params = "; Path=/; Secure; HttpOnly";
+}
